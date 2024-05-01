6896ce9fa89b62e2c329e1978866d538bb603e57
Rohankajale <rohankajale0@gmail.com>
2024-05-01 11:00:21 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 const radiatorRoutes = require('./routes/radiators')
 // express app
 const app = express()
