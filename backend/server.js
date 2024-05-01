5930df0c2dac8026b327c880c7d41d13b2f81873
Rohankajale <rohankajale0@gmail.com>
2024-05-01 16:05:49 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/radiators', radiatorRoutes)
-app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
